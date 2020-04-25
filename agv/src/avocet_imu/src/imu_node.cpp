62f588b558810498bc13bf7401cf66ea502518c6
poifull10 <poifull10@gmail.com>
2020-04-25 18:31:15 +0900

    update node name

--- agv/src/avocet_imu/src/imu_node.cpp
+++ agv/src/avocet_imu/src/imu_node.cpp
@@ -1,13 +1,12 @@
 #include <ros/ros.h>
 #include <sensor_msgs/Imu.h>
-#include <vector>
 #include <string>
+#include <vector>
 
-int main(int argc, char** argv)
-{
-  float fps=100;
-  std::string publishTopic="/imu";
-  ros::init(argc, argv, "image_publisher");
+int main(int argc, char **argv) {
+  float fps = 100;
+  std::string publishTopic = "/imu";
+  ros::init(argc, argv, "imu_publisher");
   ros::NodeHandle nh("~");
   nh.getParam("fps", fps);
   nh.getParam("publish_topic", publishTopic);
@@ -17,14 +16,14 @@ int main(int argc, char** argv)
   ros::Rate loop_rate(fps);
   ROS_INFO_STREAM("IMU fps = " << fps << ", publish topic = " << publishTopic);
   while (nh.ok()) {
-  //   cap >> frame;
-  //   if(!frame.empty()) {
-  //     auto header = std_msgs::Header();
-  //     header.stamp = ros::Time::now();
-  //     ROS_INFO_STREAM("Time " << header.stamp);
-  //     auto msg = cv_bridge::CvImage(header, "bgr8", frame).toImageMsg();
-  //     pub.publish(std::move(msg));
-  //   }
+    //   cap >> frame;
+    //   if(!frame.empty()) {
+    //     auto header = std_msgs::Header();
+    //     header.stamp = ros::Time::now();
+    //     ROS_INFO_STREAM("Time " << header.stamp);
+    //     auto msg = cv_bridge::CvImage(header, "bgr8", frame).toImageMsg();
+    //     pub.publish(std::move(msg));
+    //   }
     ros::spinOnce();
     loop_rate.sleep();
   }
