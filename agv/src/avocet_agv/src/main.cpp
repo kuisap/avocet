7b13dc56ac05e9c03b70dbb8e94b9be967068afd
poifull10 <poifull10@gmail.com>
2020-06-07 02:57:47 +0900

    fix

--- agv/src/avocet_agv/src/main.cpp
+++ agv/src/avocet_agv/src/main.cpp
@@ -17,45 +17,43 @@ int main(int argc, char** argv) {
   ros::NodeHandle nh("~");
   nh.getParam("config", config_str);
 
-  {
-    YAML::Node config = YAML::LoadFile(config_str);
-    if (config["image"]["width"]) {
-      width = config["image"]["width"].as<int>();
-    }
-
-    if (config["image"]["height"]) {
-      height = config["image"]["height"].as<int>();
-    }
-
-    if (config["image"]["fps"]) {
-      fps = config["image"]["fps"].as<float>();
-    }
-
-    if (config["image"]["publishTopic"]) {
-      publishTopic = config["image"]["publishTopic"].as<std::string>();
-    }
-
-    image_transport::ImageTransport it(nh);
-    image_transport::Publisher pub = it.advertise(publishTopic, 1);
-    cv::VideoCapture cap(0);
-    if (!cap.isOpened()) {
-      ROS_ERROR_STREAM("Camera could not be opened.");
-      return 1;
-    }
-    cap.set(cv::CAP_PROP_FRAME_WIDTH, width);
-    cap.set(cv::CAP_PROP_FRAME_HEIGHT, height);
-    cap.set(cv::CAP_PROP_FPS, fps);
-
-    cv::Mat frame;
-    sensor_msgs::ImagePtr msg;
-
-    ros::Rate loop_rate(fps);
-    ROS_INFO_STREAM("WIDTH = " << cap.get(cv::CAP_PROP_FRAME_WIDTH)
-                               << ", HEIGHT = "
-                               << cap.get(cv::CAP_PROP_FRAME_HEIGHT)
-                               << ", FPS = " << cap.get(cv::CAP_PROP_FPS)
-                               << ", publish topic = " << publishTopic);
+  YAML::Node config = YAML::LoadFile(config_str);
+  if (config["image"]["width"]) {
+    width = config["image"]["width"].as<int>();
   }
+
+  if (config["image"]["height"]) {
+    height = config["image"]["height"].as<int>();
+  }
+
+  if (config["image"]["fps"]) {
+    fps = config["image"]["fps"].as<float>();
+  }
+
+  if (config["image"]["publishTopic"]) {
+    publishTopic = config["image"]["publishTopic"].as<std::string>();
+  }
+
+  image_transport::ImageTransport it(nh);
+  image_transport::Publisher pub = it.advertise(publishTopic, 1);
+  cv::VideoCapture cap(0);
+  if (!cap.isOpened()) {
+    ROS_ERROR_STREAM("Camera could not be opened.");
+    return 1;
+  }
+  cap.set(cv::CAP_PROP_FRAME_WIDTH, width);
+  cap.set(cv::CAP_PROP_FRAME_HEIGHT, height);
+  cap.set(cv::CAP_PROP_FPS, fps);
+
+  cv::Mat frame;
+  sensor_msgs::ImagePtr msg;
+
+  ros::Rate loop_rate(fps);
+  ROS_INFO_STREAM("WIDTH = " << cap.get(cv::CAP_PROP_FRAME_WIDTH)
+                             << ", HEIGHT = "
+                             << cap.get(cv::CAP_PROP_FRAME_HEIGHT)
+                             << ", FPS = " << cap.get(cv::CAP_PROP_FPS)
+                             << ", publish topic = " << publishTopic);
 
   while (nh.ok()) {
     cap >> frame;
